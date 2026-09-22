06b20dc120a3063100ff8a9fa84f3ec750a658d6
sarouloll <saraines888@gmail.com>
2026-09-22 15:00:25 -0400

    Configuration of main widget interface

--- TP1.py
+++ TP1.py
@@ -1,1 +1,36 @@
+import sys
+import json
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidget
+)
 print ("Hello world")
+
+#Récupération de mes données
+
+
+
+
+#création d'une interface
+app = QApplication([])
+
+#Configuration de l'interface du tableau visuel
+tableau = QTableWidget()
+tableau.setRowCount(len(data))
+tableau.setColumnCount(3)
+tableau.setHorizontalHeaderLabels(["Nom", "Taille", "Quantité d'éléments"])
+
+#Affichage des données
+for i in range(len(data)):
+    item = data[i]
+    tableau.setItem(i, 0, QTableWidgetItem(item["Nom"]))
+    tableau.setItem(i, 1, QTableWidgetItem(item["Taille"]))
+    tableau.setItem(i, 2, QTableWidgetItem(item["Quantité d'éléments"]))
+
+
+#
+window = QMainWindow();
+window.setCentralWidget(tableau)
+window.show()
+sys.exit(app.exec())
