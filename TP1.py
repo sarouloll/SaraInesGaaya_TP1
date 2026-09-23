492868b56126340e3a30d56a4545ec9eea773ace
sarouloll <saraines888@gmail.com>
2026-09-22 20:47:37 -0400

    Include Table with data and functional window

--- TP1.py
+++ TP1.py
@@ -3,12 +3,34 @@ import json
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
-    QTableWidget
+    QTableWidget,
+    QTableWidgetItem
 )
-print ("Hello world")
+
+json_file = sys.argv[1]
+print(json_file)
+
+try:
+    file = open(json_file)
+    data = json.load(file)
+    print(type(data))
+except:
+    print(f"Could not load data from {json_file}")
 
 #Récupération de mes données
-
+for i in data:
+    print("Keys\n")
+    for k in i.keys():
+        print(f"        - {k}")
+    print("\n")
+    print("Values\n")
+    for v in i.values():
+        print(f"        - {v}")
+    print("\n")
+    print("Items\n")
+    for e in i.items():
+        print(f"        - {e}")
+    print("\n")
 
 
 
@@ -18,18 +40,21 @@ app = QApplication([])
 #Configuration de l'interface du tableau visuel
 tableau = QTableWidget()
 tableau.setRowCount(len(data))
-tableau.setColumnCount(3)
-tableau.setHorizontalHeaderLabels(["Nom", "Taille", "Quantité d'éléments"])
+tableau.setColumnCount(6)
+tableau.setHorizontalHeaderLabels(["id", "nom", "categorie", "format", "polygones", "statut"])
+
 
 #Affichage des données
 for i in range(len(data)):
     item = data[i]
-    tableau.setItem(i, 0, QTableWidgetItem(item["Nom"]))
-    tableau.setItem(i, 1, QTableWidgetItem(item["Taille"]))
-    tableau.setItem(i, 2, QTableWidgetItem(item["Quantité d'éléments"]))
+    tableau.setItem(i, 0, QTableWidgetItem(item["id"]))
+    tableau.setItem(i, 1, QTableWidgetItem(item["nom"]))
+    tableau.setItem(i, 2, QTableWidgetItem(item["categorie"]))
+    tableau.setItem(i, 3, QTableWidgetItem(item["format"]))
+    tableau.setItem(i, 4, QTableWidgetItem(item['polygones']))
+    tableau.setItem(i, 5, QTableWidgetItem(item["statut"]))
 
 
-#
 window = QMainWindow();
 window.setCentralWidget(tableau)
 window.show()
